54ec08f6dbb6da194366c85351c7e0b6a2a50b76
Redd Caruana <47446646+caruanaredd@users.noreply.github.com>
2023-10-30 15:28:17 +0100

    Racket Behavior
    Added:
    
    + RacketBehavior script.

---
+++ Assets/Scripts/RacketBehavior.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace Pong
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class RacketBehavior : MonoBehaviour
+    {
+        [Tooltip("The Paddle speed.")]
+        [SerializeField] private float speed = 10f;
+
+        private float _direction = 0;
+        
+        // The Rigidbody component (to move this object)
+        private Rigidbody2D _rigidbody;
+
+        /// <summary>
+        /// Stores attached components.
+        /// </summary>
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        /// <summary>
+        /// Physics update.
+        /// </summary>
+        private void FixedUpdate()
+        {
+            _rigidbody.MovePosition(_rigidbody.position + Vector2.up * (_direction * speed * Time.deltaTime));
+        }
+
+        private void Update()
+        {
+            _direction = Input.GetAxisRaw("Vertical");
+        }
+    }
+}
